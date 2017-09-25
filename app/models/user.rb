6c0cef716ce1d6fbf7f6e5b8a9311f2d2e687161
Kariny <kariny.neves@tron.com.br>
2017-09-25 13:53:05 -0300

    Adding shoulda matchers for user specs.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #Atributo virtual, nao esta presente na base de dados, utilizado nos testes
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
-  before { @user = FactoryGirl.build(:user) }
 
-  it { expect(@user).to respond_to(:email) }
-  it { expect(@user).to respond_to(:name) }
-  it { expect(@user).to respond_to(:password) }
-  it { expect(@user).to respond_to(:password_confirmation) }
-  it { expect(@user).to be_valid }
+	let(:user) { build(:user) }
+
+	#Using shoulda matchers
+	it { is_expected.to validate_presence_of(:email) }
+	it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+	it { is_expected.to validate_confirmation_of(:password) }
+	it { is_expected.to allow_value("costa@nonato.com").for(:email) }
+
 end
