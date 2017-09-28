89eaa80f3932baf219e6d565edc268364b762dc2
Kariny <kariny.neves@tron.com.br>
2017-09-28 14:24:46 -0300

    Adding user authentication token.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,17 @@ class User < ApplicationRecord
   #Atributo virtual, nao esta presente na base de dados, utilizado nos testes
 
   validates_uniqueness_of :auth_token
+  before_create :generate_authentication_token!
 
   def info
   	"#{email} - #{created_at} - Token: #{Devise.friendly_token}"
+  end
+
+  def generate_authentication_token!
+  	#Enquanto houve um usuario com o mesmo token, é gerado um novo
+  	begin
+  		self.auth_token = Devise.friendly_token
+  	end while User.exists?(auth_token: auth_token)
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,10 +14,29 @@ RSpec.describe User, type: :model do
 	describe '#info' do
 		it 'returns email and created_at' do
 			user.save!
+			#Utilizando mock - friedly_token (metodo duble)
 			allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
 
 			expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: abc123xyzTOKEN")
 
+		end
+	end
+
+	describe '#generate_authentication_token!' do
+		it 'generates a unique auth token' do
+			allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+			user.generate_authentication_token!
+
+			expect(user.auth_token).to eq('abc123xyzTOKEN')
+		end
+
+		it 'generates another auth token when the current auth token already has been taken' do
+			#Criando um novo usuario no sistema e deixa-lo com o token
+			allow(Devise).to receive(:friendly_token).and_return('abc123tokenxyz', 'abc123tokenxyz', 'abcXYZ123456789')
+			existing_user = create(:user)
+			user.generate_authentication_token!
+			#Espera-se que dois usuarios nao tenham o mesmo token
+			expect(user.auth_token).not_to eq(existing_user.auth_token)
 		end
 	end
 end
